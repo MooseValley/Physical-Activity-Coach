2458f54609c0fe88d74baef0d6c2f29cf271a185
MooseValley <Moose_Software@yahoo.com.au>
2020-02-01 11:45:36 +1000

    v0.0003 - Timers

--- PhysicalActivityCoach.java
+++ PhysicalActivityCoach.java
@@ -13,16 +13,31 @@ public class PhysicalActivityCoach extends JFrame
    JLabel exerciseTimeRemainingLabel             = new JLabel ("Exercise Time Remaining:");
    JLabel nextExercisePeriodLabel                = new JLabel ("Next Exercise Period:");
 
-   JProgressBar exerciseTimeProgressBar          = new JProgressBar ();
-   JProgressBar exerciseTimeRemainingProgressBar = new JProgressBar ();
-   JProgressBar nextExercisePeriodProgressBar    = new JProgressBar ();
+   JProgressBar exerciseTimerProgressBar              = new JProgressBar ();
+   JProgressBar exercisingTimerProgressBar            = new JProgressBar ();
+   JProgressBar exercisePeriodDelayTimerProgressBar   = new JProgressBar ();
 
    JButton nowButton                             = new JButton ("Now");
    JButton doneButton                            = new JButton ("Done");
    JButton aboutButton                           = new JButton ("About");
    JButton exitButton                            = new JButton ("Exit");
 
+
+   Timer exercisePeriodDelayTimer                = new Timer (20_000,
+                                                   event -> startExercisePeriod () );
+   Timer exercisingTimer                         = new Timer (9_000,
+                                                   event -> doExercises () );
+   Timer exerciseTimer                           = new Timer (3_000,
+                                                   event -> doAnExercise () );
+   Timer secondsCountdownTimer                   = new Timer (1_000,
+                                                   event -> refreshGUI () );
+
+
+
    // Class data:
+
+   private int exerciseCount;
+
 
    public PhysicalActivityCoach ()
    {
@@ -37,13 +52,13 @@ public class PhysicalActivityCoach extends JFrame
       flow01Panel.add (new JLabel(""));
 
       flow02Panel.add (exerciseTimeLabel);
-      flow02Panel.add (exerciseTimeProgressBar);
+      flow02Panel.add (exerciseTimerProgressBar);
 
       flow03Panel.add (exerciseTimeRemainingLabel);
-      flow03Panel.add (exerciseTimeRemainingProgressBar);
+      flow03Panel.add (exercisingTimerProgressBar);
 
       flow04Panel.add (nextExercisePeriodLabel);
-      flow04Panel.add (nextExercisePeriodProgressBar);
+      flow04Panel.add (exercisePeriodDelayTimerProgressBar);
 
       gridPanel.add (flow01Panel);
       gridPanel.add (flow02Panel);
@@ -64,6 +79,21 @@ public class PhysicalActivityCoach extends JFrame
       exitButton.addActionListener  (event -> exitApp() );
 
 
+      exerciseTimerProgressBar.setMinimum (0);
+      exerciseTimerProgressBar.setMaximum (exerciseTimer.getDelay() / 1000);
+      exerciseTimerProgressBar.setValue   (exerciseTimerProgressBar.getMaximum() );
+
+      exercisingTimerProgressBar.setMinimum (0);
+      exercisingTimerProgressBar.setMaximum (exercisingTimer.getDelay() / 1000);
+      exercisingTimerProgressBar.setValue   (exercisingTimerProgressBar.getMaximum() );
+
+      exercisePeriodDelayTimerProgressBar.setMinimum (0);
+      exercisePeriodDelayTimerProgressBar.setMaximum (exercisePeriodDelayTimer.getDelay() / 1000);
+      exercisePeriodDelayTimerProgressBar.setValue   (exercisePeriodDelayTimerProgressBar.getMaximum() );
+
+      exercisePeriodDelayTimer.start();
+      secondsCountdownTimer.start();
+
    }
 
    private void aboutApp()
@@ -77,6 +107,71 @@ public class PhysicalActivityCoach extends JFrame
    private void exitApp()
    {
       System.exit (0); // All OK
+   }
+
+   private void startExercisePeriod ()
+   {
+      // A new exercise period is starting ...
+      // We want to start the Exercises Timer
+
+      exerciseCount = 0;
+
+      exercisingTimer.start();
+      exerciseTimer.start();
+
+      exercisePeriodDelayTimer.stop();
+      exercisePeriodDelayTimerProgressBar.setValue (exercisePeriodDelayTimerProgressBar.getMaximum() );
+   }
+
+   private void doExercises ()
+   {
+      // When this timers fires, it's the END of the exercise period.
+
+      exercisingTimer.stop();
+      exerciseTimer.stop();
+
+      exerciseTimerProgressBar.setValue   (exerciseTimerProgressBar.getMaximum() );
+      exercisingTimerProgressBar.setValue (exercisingTimerProgressBar.getMaximum() );
+
+      exercisePeriodDelayTimer.start();
+   }
+
+   private void doAnExercise ()
+   {
+      // Select a random exercise
+      // Display in in a label
+
+      exerciseCount++;
+      titleLabel.setText ("Exercise #" + exerciseCount + " ...");
+
+      exerciseTimerProgressBar.setValue   (exerciseTimerProgressBar.getMaximum() );
+   }
+
+   private void refreshGUI ()
+   {
+      // Update timer progress bars.
+      int val = 0;
+
+      if (exercisePeriodDelayTimer.isRunning() == true)
+      {
+         val = exercisePeriodDelayTimerProgressBar.getValue ();
+         val--;
+         exercisePeriodDelayTimerProgressBar.setValue (val);
+      }
+
+      if (exercisingTimer.isRunning() == true)
+      {
+         val = exerciseTimerProgressBar.getValue ();
+         val--;
+         exerciseTimerProgressBar.setValue (val);
+      }
+
+      if (exerciseTimer.isRunning() == true)
+      {
+         val = exercisingTimerProgressBar.getValue ();
+         val--;
+         exercisingTimerProgressBar.setValue (val);
+      }
    }
 
    public static void main (String[] args)
